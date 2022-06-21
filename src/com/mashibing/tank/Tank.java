e44926a452907b9be4beb293a9c7b718918f7540
nelsonwq <2501551375@qq.com>
2022-06-21 20:07:59 +0800

    坦克大战1.0

---
+++ src/com/mashibing/tank/Tank.java
@@ -0,0 +1,178 @@
+package com.mashibing.tank;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.Random;
+
+import com.mashibing.tank.abstractfactory.BaseTank;
+
+public class Tank extends BaseTank{
+	private static final int SPEED = 2;
+	public static int WIDTH = ResourceMgr.goodTankU.getWidth();
+
+	public static int HEIGHT = ResourceMgr.goodTankU.getHeight();
+	
+	public Rectangle rect = new Rectangle();
+	
+	private Random random = new Random();
+
+	int x, y;
+
+	Dir dir = Dir.DOWN;
+
+	private boolean moving = true;
+	TankFrame tf = null;
+	private boolean living = true;
+	Group group = Group.BAD;
+	
+	FireStrategy fs = null;
+	
+	public Tank(int x, int y, Dir dir, Group group, TankFrame tf) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.dir = dir;
+		this.group = group;
+		this.tf = tf;
+		
+		rect.x = this.x;
+		rect.y = this.y;
+		rect.width = WIDTH;
+		rect.height = HEIGHT;
+		
+		if (group == Group.GOOD) {
+			String goodFSName = (String)PropertyMgr.get("goodFS");
+			try {
+				fs = (FireStrategy)Class.forName(goodFSName).newInstance();
+			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			String badFSName = (String)PropertyMgr.get("badFS");
+			try {
+				fs = (FireStrategy)Class.forName(badFSName).newInstance();
+			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void fire() {
+		fs.fire(this);
+	}
+	
+	public Dir getDir() {
+		return dir;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	
+	public Group getGroup() {
+		return group;
+	}
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+	public int getY() {
+		return y;
+	}
+
+	public boolean isMoving() {
+		return moving;
+	}
+
+	private void move() {
+		
+		if(!moving) return ;
+		
+		switch (dir) {
+		case LEFT:
+			x -= SPEED;
+			break;
+		case UP:
+			y -= SPEED;
+			break;
+		case RIGHT:
+			x += SPEED;
+			break;
+		case DOWN:
+			y += SPEED;
+			break;
+		}
+		
+		if(this.group == Group.BAD && random.nextInt(100) > 95) 
+			this.fire();
+		
+		if(this.group == Group.BAD && random.nextInt(100) > 95)
+			randomDir();
+		
+		boundsCheck();
+		//update rect
+		rect.x = this.x;
+		rect.y = this.y;
+		
+	}
+
+	private void boundsCheck() {
+		if (this.x < 2) x = 2;
+		if (this.y < 28) y = 28;
+		if (this.x > TankFrame.GAME_WIDTH- Tank.WIDTH -2) x = TankFrame.GAME_WIDTH - Tank.WIDTH -2;
+		if (this.y > TankFrame.GAME_HEIGHT - Tank.HEIGHT -2 ) y = TankFrame.GAME_HEIGHT -Tank.HEIGHT -2;
+	}
+	
+	private void randomDir() {
+		
+		this.dir = Dir.values()[random.nextInt(4)];
+	}
+	
+	public void paint(Graphics g) {
+		if(!living) tf.tanks.remove(this);
+		
+		
+		
+		switch(dir) {
+		case LEFT:
+			g.drawImage(this.group == Group.GOOD? ResourceMgr.goodTankL : ResourceMgr.badTankL, x, y, null);
+			break;
+		case UP:
+			g.drawImage(this.group == Group.GOOD? ResourceMgr.goodTankU : ResourceMgr.badTankU, x, y, null);
+			break;
+		case RIGHT:
+			g.drawImage(this.group == Group.GOOD? ResourceMgr.goodTankR : ResourceMgr.badTankR, x, y, null);
+			break;
+		case DOWN:
+			g.drawImage(this.group == Group.GOOD? ResourceMgr.goodTankD : ResourceMgr.badTankD, x, y, null);
+			break;
+		}
+	
+		move();
+	
+	}
+
+
+	public void setDir(Dir dir) {
+		this.dir = dir;
+	}
+
+	public void setMoving(boolean moving) {
+		this.moving = moving;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	public void die() {
+		this.living = false;
+	}
+	
+	
+
+}
